e11b1d107fbe711436988df70c8eda7c1aef3764
John Harper <john@harper.link>
2022-08-09 17:48:23 -0500

    be sure to clear the carry before using adc!

--- exercises/04-09.asm
+++ exercises/04-09.asm
@@ -19,6 +19,7 @@ Start:
     lda #1     ; Initialize the A register with the decimal value 1
 
 Loop:
+    clc
     adc #1     ; Increment A
     cmp #10    ; Compare the value in A with the decimal value 10
     bne Loop   ; Branch back to loop if the comparison was not equal (to zero)
